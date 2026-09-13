e0e05713b0b9c84add1589d804aa062ae4d032f9
Валерия Солдатова <valeriasoldatova80@gmail.com>
2026-09-13 23:55:21 +0500

    Task2

---
+++ Tyuiu.SoldatovaVP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SoldatovaVP.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SoldatovaVP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Валерия";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Валерия", res);
+        }
+    }
+}
