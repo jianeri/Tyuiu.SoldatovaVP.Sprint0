82b4114f6c8ea8c95bf6fbb33b7e51bd5cbd8bf6
Валерия Солдатова <valeriasoldatova80@gmail.com>
2026-09-14 00:14:03 +0500

    Task3

---
+++ Tyuiu.SoldatovaVP.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SoldatovaVP.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.SoldatovaVP.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
