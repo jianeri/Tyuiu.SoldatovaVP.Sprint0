a09a0331c0328756346e6053d20a25d68baf94ef
Валерия Солдатова <valeriasoldatova80@gmail.com>
2026-09-16 02:01:35 +0500

    Task7

---
+++ Tyuiu.SoldatovaVP.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.SoldatovaVP.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArrays = new int[5];
+            for (var i = 0; i < resultArrays.Length; i++)
+            {
+                resultArrays[i] = numOne[i] + numTwo[i];
+            }
+            return resultArrays;
+        }
+    }
+}
