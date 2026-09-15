6569d037cb37fb87de52c64667a1f62c975b478e
Валерия Солдатова <valeriasoldatova80@gmail.com>
2026-09-16 01:11:33 +0500

    Task6

---
+++ Tyuiu.SoldatovaVP.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SoldatovaVP.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.SoldatovaVP.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность эдементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+
+}
